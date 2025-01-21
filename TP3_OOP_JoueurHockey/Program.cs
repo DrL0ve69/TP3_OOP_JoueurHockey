8f7505ba0e6e31de4b8009b308d265d0f9cdc1b1
DrL0ve69 <146204861+DrL0ve69@users.noreply.github.com>
2025-01-21 10:38:32 -0500

    Tp terminado! Manque les tests unitaires!

--- TP3_OOP_JoueurHockey/Program.cs
+++ TP3_OOP_JoueurHockey/Program.cs
@@ -17,5 +17,21 @@ foreach(JoueursHockey joueur in listeJoueurs)Console.WriteLine(joueur.ToString()
 
 // Test de la méthode Copy() :
 
-JoueursHockey joueur1Prime = joueur1;
-Console.WriteLine($"Voici une copie du joueur 1 :\n{joueur1Prime.ToString()}");
+JoueursHockey joueur1Prime = joueur1.CopierJoueur();
+Console.WriteLine($"Voici la copie du joueur 1 : {joueur1Prime}");
+
+Console.WriteLine($"Modifications du joueurPrime, et vérifier si cela affecte aussi la copie originale : \n");
+
+joueur1Prime.Position = "Centre"; joueur1Prime.NbPartiesJouees = 666; joueur1Prime.NbButs = 1000;
+
+Console.WriteLine($"Voici les modifications du joueurPrime suivi du joueur1 :\n" +
+    $"{joueur1Prime}{joueur1}");
+
+// Test des valeurs pour la méthode Equal()
+
+Console.WriteLine($"Le joueur 1 devrait être = au joueurPrime . Vérifions :\n" +
+    $"Réponse : {joueur1.Equals(joueur1Prime)}\n");
+
+Console.WriteLine("Vérifions avec les autres :" + "\n");
+
+foreach(JoueursHockey joueur in listeJoueurs)Console.WriteLine($"Réponse : {joueur1.Equals(joueur)}\n");
