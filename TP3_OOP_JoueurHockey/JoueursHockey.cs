8596c5d382743f483cfad9650eaed973ed5b4246
DrL0ve69 <146204861+DrL0ve69@users.noreply.github.com>
2025-01-21 09:12:51 -0500

    Ajoutez des fichiers projet.

---
+++ TP3_OOP_JoueurHockey/JoueursHockey.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_OOP_JoueurHockey
+{
+    public class JoueursHockey
+    {
+        public string Nom { get; set; } = string.Empty;
+        public string Prenom { get; set; } = string.Empty;
+        public int Numero { get; set; } = 0;
+        public string Position { get; set; } = "Donnée non-disponible";
+        public int NbButs { get; set; } = 0;
+        public int NbPartiesJouees { get; set; } = 0;
+        public JoueursHockey(string nom, string prenom, int numero)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Numero = numero;
+            //Console.WriteLine(ToString());
+        }
+        public JoueursHockey(string nom, string prenom, int numero, string position, int nbButs, int nbPartiesJouees) : this(nom, prenom, numero)
+        {
+            Position = position;
+            NbButs = nbButs;
+            NbPartiesJouees = nbPartiesJouees;
+            //Console.WriteLine(ToString());
+        }
+        public override string ToString()
+        {
+            return $"\n{Nom}, {Prenom} :\n" +
+                $"Numéro: {Numero}\n" +
+                $"Position: {Position}, Nombre de buts : {NbButs}, Nombre de parties jouées : {NbPartiesJouees}\n" +
+                $"-----------------------------------------------------------------------------------------------";
+        }
+    }
+}
---
+++ TP3_OOP_JoueurHockey/Program.cs
@@ -0,0 +1,21 @@
+﻿using TP3_OOP_JoueurHockey;
+
+Console.WriteLine("TP3-JoueurHockey____________________________________________________________________\n");
+
+// Déclaration des variables (JoueursHockey)
+JoueursHockey joueur1 = new("Richard", "Maurice", 9);
+JoueursHockey joueur2 = new("Béliveau", "Jean", 4);
+JoueursHockey joueur3 = new("Dupot", "Yvan", 69, "Gardien",420, 42069);
+JoueursHockey joueur4 = new("Bozo", "LeClown", 99, "Centre", 1, 1000);
+
+// Liste de Joueurs
+List<JoueursHockey> listeJoueurs = new List<JoueursHockey>{ joueur1, joueur2, joueur3, joueur4 };
+
+// Test de la méthode ToString() :
+
+foreach(JoueursHockey joueur in listeJoueurs)Console.WriteLine(joueur.ToString());
+
+// Test de la méthode Copy() :
+
+JoueursHockey joueur1Prime = joueur1;
+Console.WriteLine($"Voici une copie du joueur 1 :\n{joueur1Prime.ToString()}");
